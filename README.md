44a359d33fa6d019560b9afa153531edd1abc71a
Shahriar Rifat <shahriarrifat0@gmail.com>
2022-03-24 12:48:25 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dummy007-Magic_Ball
+ A dummy Flutter app, created as a beginner, from my learning.
---
+++ android/app/src/main/kotlin/com/shahriar/rifat/magic_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shahriar.rifat.magic_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
